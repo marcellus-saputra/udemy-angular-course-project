2f44f27e1ddf5aa9df40e5a8484ccd09ab8b7100
Marcellus Prama Saputra <marcellussaputra@gmail.com>
2023-09-01 22:17:06 +0700

    Added recipe details

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -8,7 +8,15 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent{
   recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'Lorem ipsum','https://static01.nyt.com/images/2021/02/17/dining/17tootired-sesame-noodles/merlin_126109679_c4abf7f2-e900-4552-a09a-a5421d4cbe8f-superJumbo.jpg?quality=75&auto=webp')
+    new Recipe(
+      'Test Recipe',
+      'Lorem ipsum',
+      'https://static01.nyt.com/images/2021/02/17/dining/17tootired-sesame-noodles/merlin_126109679_c4abf7f2-e900-4552-a09a-a5421d4cbe8f-superJumbo.jpg?quality=75&auto=webp'
+      ),
+    new Recipe(
+      'Test Recipe',
+      'Lorem ipsum',
+      'https://static01.nyt.com/images/2021/02/17/dining/17tootired-sesame-noodles/merlin_126109679_c4abf7f2-e900-4552-a09a-a5421d4cbe8f-superJumbo.jpg?quality=75&auto=webp')
   ]
 
   constructor() {
